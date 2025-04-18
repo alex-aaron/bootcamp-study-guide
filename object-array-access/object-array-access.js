c7e8150e491dd609cfb5351079dda8d084484fc7
Alex Aaron <alexfrancisaaron@gmail.com>
2025-04-18 19:32:24 +0000

    added additional styling

---
+++ object-array-access/object-array-access.js
@@ -0,0 +1,18 @@
+const user = {
+  firstName: 'Stephanie',
+  lastName: 'Cooper',
+  age: 38,
+  email: 'scoop@gmail.com',
+  location: {
+    nation: 'United States of America',
+    state: 'Louisiana',
+    city: 'New Orleans'
+  },
+  purchases: [
+    {
+      product: "Ergofit Wired Earbuds",
+      price: 15.99,
+      
+    }
+  ]
+}
